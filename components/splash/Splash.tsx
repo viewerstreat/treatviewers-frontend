fbe2ba8c47afa883d36274aa81581062c843a25d
Sibaprasad Maiti <sibu.it13@gmail.com>
2022-06-15 09:36:30 +0530

    splash screen modified

--- components/splash/Splash.tsx
+++ components/splash/Splash.tsx
@@ -1,13 +1,14 @@
 import React, {useState, useEffect, useRef} from 'react';
-import {Text, ImageBackground, StyleSheet, Animated} from 'react-native';
+import {ImageBackground, StyleSheet, Animated, Text, Easing} from 'react-native';
 import type {NativeStackScreenProps} from '@react-navigation/native-stack';
-import {SPLASH_TIMEOUT} from '../../config';
+import {SPLASH_TIMEOUT} from '../../utils/config';
 import {RootStackParamList} from '../../App';
+import {COLOR_RED, COLOR_WHITE} from '../../utils/constants';
 
 type Props = NativeStackScreenProps<RootStackParamList, 'Splash'>;
 
 function Splash(props: Props) {
-  const animation = useRef(new Animated.Value(300)).current;
+  const animation = useRef(new Animated.Value(-500)).current;
   const [authLoaded, setAuthLoaded] = useState(false);
   useEffect(() => {
     setTimeout(() => {
@@ -16,10 +17,10 @@ function Splash(props: Props) {
   }, []);
 
   useEffect(() => {
-    Animated.spring(animation, {
+    Animated.timing(animation, {
       toValue: 0,
-      friction: 3,
-      tension: 100,
+      easing: Easing.in(Easing.bounce),
+      duration: 2000,
       useNativeDriver: true,
     }).start();
     // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -33,11 +34,16 @@ function Splash(props: Props) {
 
   return (
     <ImageBackground
-      source={require('../../images/bgimage.png')}
+      source={require('../../images/bg.png')}
       resizeMode="cover"
       style={styles.container}>
-      <Animated.View style={[styles.txtContainer, {transform: [{translateY: animation}]}]}>
-        <Text style={styles.txt}>Treat Viewers</Text>
+      <Animated.View style={[styles.wrapper, {transform: [{translateY: animation}]}]}>
+        <Text style={styles.txt}>
+          <Text style={styles.redText}>t</Text>
+          <Text style={styles.whiteText}>rails</Text>
+          <Text style={styles.redText}>b</Text>
+          <Text style={styles.whiteText}>uddy</Text>
+        </Text>
       </Animated.View>
     </ImageBackground>
   );
@@ -46,16 +52,22 @@ function Splash(props: Props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
     alignItems: 'center',
   },
-  txtContainer: {
-    borderColor: '#FFFFFA',
-    borderBottomWidth: 5,
+  wrapper: {
+    // backgroundColor: 'yellow',
+    paddingBottom: 50,
   },
   txt: {
-    fontSize: 36,
-    color: '#FFFFFA',
+    fontSize: 48,
+    fontWeight: 'bold',
+  },
+  redText: {
+    color: COLOR_RED,
+  },
+  whiteText: {
+    color: COLOR_WHITE,
   },
 });
 
---
+++ utils/constants.ts
@@ -0,0 +1,7 @@
+export const COLOR_RED = '#EA3333';
+export const COLOR_WHITE = '#FFFFFA';
+export const COLOR_BLUE = '#182A46';
+export const COLOR_BROWN = '#5C3439';
+export const COLOR_YELLOW = '#EEAA33';
+export const COLOR_GREY = '#DADADA';
+export const COLOR_BLACK = '#363636';
