1711dd609513b576179f1e68bd90b3d926f1dba9
Sibaprasad Maiti <sibu.it13@gmail.com>
2022-06-21 10:09:38 +0530

    app name changed

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 npx react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res
 cd android
+./gradlew clean
 ./gradlew assembleDebug
 cd app/build/outputs/apk/debug
 ls 
